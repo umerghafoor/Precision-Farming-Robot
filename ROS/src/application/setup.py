d2f15d392816b53d149942dd38a7c5bc8c465348
Umer Ghafoor <umerghaforr@gmail.com>
2025-01-26 18:13:37 +0500

    refact: add requirements.txt and update setup.py with additional dependencies

--- ROS/src/application/setup.py
+++ ROS/src/application/setup.py
@@ -11,7 +11,17 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'PyQt6',
+        'opencv-python',
+        'pygame',
+        'rclpy',
+        'numpy',
+        'pyyaml',
+        'pyserial',
+        'Cython'
+        ],
     zip_safe=True,
     maintainer='umerghafoor',
     maintainer_email='umerghafoor@todo.todo',
@@ -20,6 +30,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'robot_control_app = application.main:main',
         ],
     },
 )
